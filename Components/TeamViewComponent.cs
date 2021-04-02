04c28803d2d05f0d481a70e3256617a9287f1dc7
MitchRogers <mitchellmel@outlook.com>
2021-04-02 10:54:48 -0600

    Add project files.

---
+++ Components/TeamViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Assignment10.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment10.Components
+{
+    public class TeamViewComponent : ViewComponent
+    {
+        private BowlingLeagueContext _context;
+        public TeamViewComponent(BowlingLeagueContext con)
+        {
+            _context = con;
+        }
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedTeam = RouteData?.Values["teamName"];
+
+            return View(_context.Teams
+                        .Distinct()
+                        .OrderBy(x => x));
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Assignment10.Models;
+using Assignment10.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment10.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private BowlingLeagueContext _context { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, BowlingLeagueContext con)
+        {
+            _logger = logger;
+            _context = con;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ContactList(long? team, string teamName, int pageNum = 0)
+        {
+            int bowlersPerPage = 5;
+
+            return View(new ContactListViewModel
+            {
+                Bowlers = (_context.Bowlers
+                        .Where(b => b.TeamId == team || team == null)
+                        .OrderBy(b => b.BowlerFirstName)
+                        .Skip((pageNum - 1) * bowlersPerPage)
+                        .Take(bowlersPerPage)
+                        .ToList()),
+
+                PageNumberingInfo = new PageNumberingInfo
+                {
+                    NumBowlersPerPage = bowlersPerPage,
+                    CurrentPage = pageNum,
+                    TotalBowlers = (team == null ? _context.Bowlers.Count() :
+                        _context.Bowlers.Where(x => x.TeamId == team).Count())
+                },
+
+                TeamName = teamName
+            });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
